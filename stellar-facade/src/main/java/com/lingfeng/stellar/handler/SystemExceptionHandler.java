4f93845daa42fe6a78e6d680cbfe1888b318d400
xiaolingfeng <v_xiaolingfeng01@baidu.com>
2025-04-07 20:52:03 +0800

    connect database

--- stellar-facade/src/main/java/com/lingfeng/stellar/handler/SystemExceptionHandler.java
+++ stellar-facade/src/main/java/com/lingfeng/stellar/handler/SystemExceptionHandler.java
@@ -2,6 +2,7 @@ package com.lingfeng.stellar.handler;
 
 import com.lingfeng.stellar.response.HttpResponse;
 import com.lingfeng.stellar.response.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +14,13 @@ import java.util.Map;
  * @create 2024/5/27
  */
 
+@Slf4j
 @ResponseBody
 @ControllerAdvice
 public class SystemExceptionHandler {
     @ExceptionHandler(Exception.class)
     Map<String,Object> systemErrorHandler(Exception e){
+        log.error("系统异常",e);
         return HttpResponse.fail(ResponseCode.SYSTEM_ERROR.getCode(),ResponseCode.SYSTEM_ERROR.getMsg());
     }
 }
